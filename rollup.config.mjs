12d0204469c0b98dd5846b8e2ac460b49075809e
cperez120 <57533711+cperez120@users.noreply.github.com>
2026-03-30 13:28:25 -0500

    Rename rollup.config.js to rollup.config.mjs
    svelte 5 refactoring

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,11 +6,12 @@ import postcss from "rollup-plugin-postcss"
 import svg from "rollup-plugin-svg"
 import json from "rollup-plugin-json"
 import nodePolyfills from "rollup-plugin-polyfill-node"
-import copy from "rollup-plugin-copy2"
+import copy2 from "rollup-plugin-copy2"
 import tar from "tar"
 import fs from "fs"
-import pkg from "./package.json"
+const pkg = JSON.parse(fs.readFileSync(new URL('./package.json', import.meta.url), 'utf8'))
 import crypto from "crypto"
+import { validate } from "@budibase/backend-core/plugins"
 
 const ignoredWarnings = [
   "unused-export-let",
@@ -69,23 +70,41 @@ const bundle = () => ({
   },
 })
 
+const validateSchema = () => ({
+  buildStart() {
+    const schema = fs.readFileSync("schema.json", "utf8")
+    validate(JSON.parse(schema))
+  }
+})
+
 export default {
   input: "index.js",
+  external: (id) => id === "svelte" || id.startsWith("svelte/"),
   output: {
-    sourcemap: false,
+    sourcemap: process.env.ROLLUP_WATCH ? "inline" : false,
     format: "iife",
     file: "dist/plugin.min.js",
     name: "plugin",
-    globals: {
-      svelte: "svelte",
-      "svelte/internal": "svelte_internal",
+    globals: (id) => {
+      if (id === "svelte/store") return "svelteStore"
+      if (id === "svelte/transition") return "svelteTransition"
+      if (id === "svelte/animate") return "svelteAnimate"
+      if (id === "svelte/motion") return "svelteMotion"
+      if (id === "svelte/easing") return "svelteEasing"
+      if (id.includes("/internal")) return "svelteInternal"
+      return "svelte"
     },
   },
-  external: ["svelte", "svelte/internal"],
   plugins: [
+    validateSchema(),
     clean(),
     svelte({
       emitCss: true,
+      compilerOptions: {
+        compatibility: {
+          componentApi: 4,
+        },
+      },
       onwarn: (warning, handler) => {
         // Ignore some warnings
         if (!ignoredWarnings.includes(warning.code)) {
@@ -99,12 +118,11 @@ export default {
     resolve({
       preferBuiltins: true,
       browser: true,
-      skip: ["svelte", "svelte/internal"],
     }),
     svg(),
     json(),
     terser(),
-    copy({
+    copy2.default({
       assets: ["schema.json", "package.json"],
     }),
     hash(),
